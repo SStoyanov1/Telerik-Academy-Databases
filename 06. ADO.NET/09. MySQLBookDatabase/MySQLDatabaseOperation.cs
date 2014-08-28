5bdd227b15d7f6ac1fdb748055d8e2b38e7d4ab0
SStoyanov1 <hehormajla@gmail.com>
2014-08-28 15:50:39 +0300

    ado.net homework added

---
+++ 06. ADO.NET/09. MySQLBookDatabase/MySQLDatabaseOperation.cs
@@ -0,0 +1,85 @@
+﻿//09. Download and install MySQL database, MySQL Connector/Net (.NET Data Provider for MySQL) +
+//MySQL Workbench GUI administration tool . Create a MySQL database to store Books (title, author,
+//publish date and ISBN). Write methods for listing all books, finding a book by name and adding a book.
+
+//IMPORTANT: USE THE books-database.sql FILE TO GENERATE THE BOOKS TABLE
+
+namespace _09.MySQLBookDatabase
+{
+    using System;
+    using MySql.Data.MySqlClient;
+
+    public class MySQLDatabaseOperation
+    {
+        static void Main(string[] args)
+        {
+            string mySqlConnectionString = @"Server=localhost;Port=3306;" +
+                "Database=mydb;Uid=root;Pwd=Az9007317565;";
+
+            MySqlConnection bookDbCon = new MySqlConnection(mySqlConnectionString);
+            string title = "Les miserables";
+            DateTime publishDate = new DateTime(1862, 12, 1);
+            string iSBN = "6789";
+
+            //AddBookToDatabase(title, publishDate, iSBN, bookDbCon);
+            ListAllBooks(bookDbCon);
+            Console.WriteLine();
+
+            string titleToFind = "Lord";
+            FindBookByName(bookDbCon, titleToFind);
+        }
+
+        private static void AddBookToDatabase(string title, DateTime publishDate, string ISBN, MySqlConnection addBookCon)
+        {
+            MySqlCommand cmdInsertBook = new MySqlCommand(
+                "INSERT INTO books (title, publishDate, ISBN) " +
+                "VALUES (@title, @publishDate, @ISBN)", addBookCon);
+            addBookCon.Open();
+            cmdInsertBook.Parameters.AddWithValue("@title", title);
+            cmdInsertBook.Parameters.AddWithValue("@publishDate", publishDate);
+            cmdInsertBook.Parameters.AddWithValue("@ISBN", ISBN);
+            cmdInsertBook.ExecuteNonQuery();
+            addBookCon.Close();
+        }
+
+        private static void ListAllBooks(MySqlConnection listBooksCon)
+        {
+            MySqlCommand cmdListBooks = new MySqlCommand(
+                "SELECT BookId, Title, PublishDate, ISBN FROM Books",
+                listBooksCon);
+
+            listBooksCon.Open();
+            MySqlDataReader reader = cmdListBooks.ExecuteReader();
+
+            using (reader)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0} | {1} | {2} | {3}", reader[0], reader[1], reader[2], reader[3]);
+                }
+            }
+            listBooksCon.Close();
+        }
+
+        private static void FindBookByName(MySqlConnection connection, string title)
+        {
+            string insertSql = "SELECT BookId, Title, PublishDate, ISBN FROM Books WHERE Title LIKE @bookName";
+
+            MySqlCommand insertBook = new MySqlCommand(insertSql, connection);
+
+            insertBook.Parameters.AddWithValue("@bookName", "%" + title + "%");
+
+            connection.Open();
+
+            using (MySqlDataReader reader = insertBook.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0} | {1} | {2} | {3}", reader[0], reader[1], reader[2], reader[3]);
+                }
+            }
+
+            connection.Close();
+        }
+    }
+}
---
+++ 06. ADO.NET/10. SQLiteBookDBOperations/SQLiteDBOperations.cs
@@ -0,0 +1,74 @@
+﻿//10. Re-implement the previous task with SQLite embedded DB (see http://sqlite.phxsoftware.com).
+
+namespace _10.SQLiteBookDBOperations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Data.SQLite;
+
+    public class SQLiteDBOperations
+    {
+        static void Main(string[] args)
+        {
+            var sQLiteCon = new SQLiteConnection("Data Source=..\\..\\..\\books-database.s3db;Version=3;");
+            //AddBooks("Imperial Bedrooms", "2345", sQLiteCon);
+            ListAllBooks(sQLiteCon);
+
+            string titleToFind = "Lord";
+            FindBookByName(sQLiteCon, titleToFind);
+        }
+
+        private static void FindBookByName(SQLiteConnection findBookCon, string title)
+        {
+            findBookCon.Open();
+
+            string findBookSql = "SELECT BookId, Title, ISBN FROM Books WHERE Title LIKE @bookName";
+
+            SQLiteCommand cmdFindBook = new SQLiteCommand(findBookSql, findBookCon);
+            cmdFindBook.Parameters.AddWithValue("@bookName", "%" + title + "%" as string);
+
+            SQLiteDataReader reader = cmdFindBook.ExecuteReader();
+
+            using (reader)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0} | {1} | {2}", reader[0], reader[1], reader[2]);
+                }
+            }
+            findBookCon.Close();
+        }
+
+        private static void AddBooks(string title, string isbn, SQLiteConnection addCon)
+        {
+            addCon.Open();
+            string sql = "insert into Books (Title, ISBN) values (@title, @isbn)";
+            SQLiteCommand command = new SQLiteCommand(sql, addCon);
+            command.Parameters.AddWithValue("@title", title as string);
+            command.Parameters.AddWithValue("@isbn", isbn as string);
+            command.ExecuteNonQuery();
+            addCon.Close();
+        }
+
+        private static void ListAllBooks(SQLiteConnection listBooksCon)
+        {
+            listBooksCon.Open();
+
+            string listBooksSql = "SELECT BookId, Title, ISBN FROM Books";
+
+            SQLiteCommand cmdListBooks = new SQLiteCommand(listBooksSql, listBooksCon);
+
+            SQLiteDataReader reader = cmdListBooks.ExecuteReader();
+
+            using (reader)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0} | {1} | {2}", reader[0], reader[1], reader[2]);
+                }
+            }
+            listBooksCon.Close();
+        }
+    }
+}
